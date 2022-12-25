f841ce3a8c738d6558a87db62bf63525936420e9
rasha026 <78126683+rasha026@users.noreply.github.com>
2022-12-25 10:29:15 +0200

    no message

---
+++ MBD_Calculator/programming_calculator/programming_calculator_ert_rtw/html/programming_calculator_traceInfo.js
@@ -0,0 +1,76 @@
+function RTW_Sid2UrlHash() {
+	this.urlHashMap = new Array();
+	/* <Root>/In1 */
+	this.urlHashMap["programming_calculator:5"] = "programming_calculator.c:37,45,53,61,68&programming_calculator.h:41";
+	/* <Root>/In2 */
+	this.urlHashMap["programming_calculator:6"] = "programming_calculator.c:38,46,54,62,74&programming_calculator.h:42";
+	/* <Root>/Logical
+Operator */
+	this.urlHashMap["programming_calculator:1"] = "programming_calculator.c:39";
+	/* <Root>/Logical
+Operator1 */
+	this.urlHashMap["programming_calculator:2"] = "programming_calculator.c:47";
+	/* <Root>/Logical
+Operator2 */
+	this.urlHashMap["programming_calculator:3"] = "programming_calculator.c:55";
+	/* <Root>/Logical
+Operator3 */
+	this.urlHashMap["programming_calculator:4"] = "msg=rtwMsg_CodeGenerationReducedBlock&block=programming_calculator:4";
+	/* <Root>/Logical
+Operator4 */
+	this.urlHashMap["programming_calculator:22"] = "programming_calculator.c:69";
+	/* <Root>/Logical
+Operator5 */
+	this.urlHashMap["programming_calculator:23"] = "programming_calculator.c:75";
+	/* <Root>/Out1 */
+	this.urlHashMap["programming_calculator:7"] = "programming_calculator.c:36&programming_calculator.h:47";
+	/* <Root>/Out2 */
+	this.urlHashMap["programming_calculator:8"] = "programming_calculator.c:44&programming_calculator.h:48";
+	/* <Root>/Out3 */
+	this.urlHashMap["programming_calculator:9"] = "programming_calculator.c:52&programming_calculator.h:49";
+	/* <Root>/Out4 */
+	this.urlHashMap["programming_calculator:10"] = "programming_calculator.c:60&programming_calculator.h:50";
+	/* <Root>/Out5 */
+	this.urlHashMap["programming_calculator:24"] = "programming_calculator.c:67&programming_calculator.h:51";
+	/* <Root>/Out6 */
+	this.urlHashMap["programming_calculator:25"] = "programming_calculator.c:73&programming_calculator.h:52";
+	this.getUrlHash = function(sid) { return this.urlHashMap[sid];}
+}
+RTW_Sid2UrlHash.instance = new RTW_Sid2UrlHash();
+function RTW_rtwnameSIDMap() {
+	this.rtwnameHashMap = new Array();
+	this.sidHashMap = new Array();
+	this.rtwnameHashMap["<Root>"] = {sid: "programming_calculator"};
+	this.sidHashMap["programming_calculator"] = {rtwname: "<Root>"};
+	this.rtwnameHashMap["<Root>/In1"] = {sid: "programming_calculator:5"};
+	this.sidHashMap["programming_calculator:5"] = {rtwname: "<Root>/In1"};
+	this.rtwnameHashMap["<Root>/In2"] = {sid: "programming_calculator:6"};
+	this.sidHashMap["programming_calculator:6"] = {rtwname: "<Root>/In2"};
+	this.rtwnameHashMap["<Root>/Logical Operator"] = {sid: "programming_calculator:1"};
+	this.sidHashMap["programming_calculator:1"] = {rtwname: "<Root>/Logical Operator"};
+	this.rtwnameHashMap["<Root>/Logical Operator1"] = {sid: "programming_calculator:2"};
+	this.sidHashMap["programming_calculator:2"] = {rtwname: "<Root>/Logical Operator1"};
+	this.rtwnameHashMap["<Root>/Logical Operator2"] = {sid: "programming_calculator:3"};
+	this.sidHashMap["programming_calculator:3"] = {rtwname: "<Root>/Logical Operator2"};
+	this.rtwnameHashMap["<Root>/Logical Operator3"] = {sid: "programming_calculator:4"};
+	this.sidHashMap["programming_calculator:4"] = {rtwname: "<Root>/Logical Operator3"};
+	this.rtwnameHashMap["<Root>/Logical Operator4"] = {sid: "programming_calculator:22"};
+	this.sidHashMap["programming_calculator:22"] = {rtwname: "<Root>/Logical Operator4"};
+	this.rtwnameHashMap["<Root>/Logical Operator5"] = {sid: "programming_calculator:23"};
+	this.sidHashMap["programming_calculator:23"] = {rtwname: "<Root>/Logical Operator5"};
+	this.rtwnameHashMap["<Root>/Out1"] = {sid: "programming_calculator:7"};
+	this.sidHashMap["programming_calculator:7"] = {rtwname: "<Root>/Out1"};
+	this.rtwnameHashMap["<Root>/Out2"] = {sid: "programming_calculator:8"};
+	this.sidHashMap["programming_calculator:8"] = {rtwname: "<Root>/Out2"};
+	this.rtwnameHashMap["<Root>/Out3"] = {sid: "programming_calculator:9"};
+	this.sidHashMap["programming_calculator:9"] = {rtwname: "<Root>/Out3"};
+	this.rtwnameHashMap["<Root>/Out4"] = {sid: "programming_calculator:10"};
+	this.sidHashMap["programming_calculator:10"] = {rtwname: "<Root>/Out4"};
+	this.rtwnameHashMap["<Root>/Out5"] = {sid: "programming_calculator:24"};
+	this.sidHashMap["programming_calculator:24"] = {rtwname: "<Root>/Out5"};
+	this.rtwnameHashMap["<Root>/Out6"] = {sid: "programming_calculator:25"};
+	this.sidHashMap["programming_calculator:25"] = {rtwname: "<Root>/Out6"};
+	this.getSID = function(rtwname) { return this.rtwnameHashMap[rtwname];}
+	this.getRtwname = function(sid) { return this.sidHashMap[sid];}
+}
+RTW_rtwnameSIDMap.instance = new RTW_rtwnameSIDMap();
---
+++ MBD_Calculator/scientific_calculator/scientific_calculator_ert_rtw/scientific_calculator_types.h
@@ -0,0 +1,31 @@
+/*
+ * File: scientific_calculator_types.h
+ *
+ * Code generated for Simulink model 'scientific_calculator'.
+ *
+ * Model version                  : 1.2
+ * Simulink Coder version         : 8.11 (R2016b) 25-Aug-2016
+ * C/C++ source code generated on : Sun Dec 25 10:24:47 2022
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives:
+ *    1. Execution efficiency
+ *    2. RAM efficiency
+ *    3. ROM efficiency
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_scientific_calculator_types_h_
+#define RTW_HEADER_scientific_calculator_types_h_
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_scientific_calculator_T RT_MODEL_scientific_calculato_T;
+
+#endif                                 /* RTW_HEADER_scientific_calculator_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
